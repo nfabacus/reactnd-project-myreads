36cb3d9c1da27ce122847a75759b124cd3202e46
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-08-26 01:05:36 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
+
+class BooksApp extends Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
